7b0b524bb3f8bb49233cef62acbb59bd13237365
Sergi Mateo <sergimateo@gmail.com>
2021-03-16 11:46:54 +0100

    building store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import movies from './modules/movies.js'
+import allMovies from './../assets/movieList.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    allMovies: allMovies
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {
+    movies
+  }
 })
