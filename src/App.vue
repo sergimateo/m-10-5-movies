965faef43a36bdcf73af27725a3ad9030fb7b191
Sergi Mateo <sergimateo@gmail.com>
2021-03-18 15:38:20 +0100

    dummy commit to see if merging workflow works wow

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <SearchBar />
     <Movies />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import Movies from './components/Movies.vue'
 import SearchBar from './components/SearchBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
     Movies,
     SearchBar
   }
